0f2d8ca5b7894347d8bc82bdb22dc96706eae102
Saamnuur <inuur65@gmail.com>
2021-05-10 23:13:00 +0000

    contact form updated so submissions go to email and a pop up message occurs when the submission is complete. css styling for contact page complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,42 @@
+//Nav Bar//
+
 //Obtained from W3Schools with some tweeking//
 
-/* When clicked you can toggle between hiding and showing the dropdown content */
+// When clicked you can toggle between hiding and showing the dropdown content //
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if clicked
-  window.onclick = function(event) {
+}
+
+// Close the dropdown menu if clicked
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
+
+
+
+//Form submission//
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        emailjs.sendForm("service_7epps9q", "template_hdvhudn", this).then(function(response) {
+            console.log("SUCCESS!", response);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+    });
+};
+
+document.getElementById("contact-form").onsubmit = function() {myFunction()};
+
+function myFunction() {
+  alert("The form was submitted");
+}
+
